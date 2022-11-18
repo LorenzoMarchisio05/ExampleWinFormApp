d6e910b18f124f1f5c5c5d6b0d26e714374dc7d2
l.marchisio.2268 <l.marchisio.2268@vallauri.edu>
2022-11-18 13:20:34 +0100

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace ExampleWinFormApp
+﻿namespace Es11_Events
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Variabile di progettazione necessaria.
@@ -29,12 +29,62 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnAccellera = new System.Windows.Forms.Button();
+            this.pbVelocità = new System.Windows.Forms.ProgressBar();
+            this.btnFrena = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAccellera
+            // 
+            this.btnAccellera.Location = new System.Drawing.Point(12, 12);
+            this.btnAccellera.Name = "btnAccellera";
+            this.btnAccellera.Size = new System.Drawing.Size(122, 23);
+            this.btnAccellera.TabIndex = 0;
+            this.btnAccellera.Text = "Accellera";
+            this.btnAccellera.UseVisualStyleBackColor = true;
+            // 
+            // pbVelocità
+            // 
+            this.pbVelocità.Location = new System.Drawing.Point(12, 60);
+            this.pbVelocità.Maximum = 10000;
+            this.pbVelocità.Name = "pbVelocità";
+            this.pbVelocità.Size = new System.Drawing.Size(250, 23);
+            this.pbVelocità.TabIndex = 2;
+            // 
+            // btnFrena
+            // 
+            this.btnFrena.Location = new System.Drawing.Point(140, 12);
+            this.btnFrena.Name = "btnFrena";
+            this.btnFrena.Size = new System.Drawing.Size(122, 23);
+            this.btnFrena.TabIndex = 3;
+            this.btnFrena.Text = "Frena";
+            this.btnFrena.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(265, 92);
+            this.Controls.Add(this.btnFrena);
+            this.Controls.Add(this.pbVelocità);
+            this.Controls.Add(this.btnAccellera);
+            this.Name = "frmMain";
+            this.Text = "Events";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAccellera;
+        private System.Windows.Forms.ProgressBar pbVelocità;
+        private System.Windows.Forms.Button btnFrena;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ExampleWinFormApp
+namespace Es11_Events
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        private int numeroGiri;
+
+        private delegate void reachedLimit();
+        private event reachedLimit reachedLimitEvent;
+
+        private delegate void aggiorna(ref int numeroGiri);
+
+        public const int INCREMENTO_GIRI = 1000;
+        private static aggiorna aggiornaHandler;
+
+        public frmMain()
         {
             InitializeComponent();
+
+            timer1.Interval = 1000;
+        }
+
+
+        public void handleCambioGiri(object sender, EventArgs e)
+        {
+            string tipoOperazione = (sender as Button).Text.ToLower();
+
+            if (tipoOperazione == "accellera")
+            {
+                aggiornaHandler = incrementa;
+            }
+            else
+            {
+                aggiornaHandler = decrementa; 
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            aggiornaHandler?.Invoke(ref numeroGiri);
+        }
+
+        private void incrementa(ref int giri)
+        {
+            giri += INCREMENTO_GIRI;
+        }
+
+        private void decrementa(ref int giri)
+        {
+            giri -= INCREMENTO_GIRI;
+        }
+
+        private void changeProgressBarState()
+        {
+            pbVelocità.Value = numeroGiri;
+            if(numeroGiri >= pbVelocità.Value)
+            {
+                
+            }
         }
     }
 }
